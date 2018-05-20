9ab5bed9b9f5cb2747a6b42ac09e66102edb6bab
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-05-20 02:54:43 -0700

    changes

--- application/views/admins/index.php
+++ application/views/admins/index.php
@@ -1,6 +1,16 @@
-<?php $this->load->view($this->config->item('theme') . 'header'); ?>
-<h1>
-    Welcome-<?=$email?>-<?=$logged?>
-</h1>
+<?php $this->load->view($this->config->item('theme') . 'header'); 
 
-<?php $this->load->view($this->config->item('theme') . 'footer'); ?>
+    
+    if ($first_name != ""){
+    echo '<img src="../img/' . $picture . '" width="100" height="100"' . '<h3>' . $email . '</h3><br>'; 
+    echo '<h1>Welcome ' . $first_name . " " . $last_name . '!<br><hr><br> You ' . $logged . " at " . date('Y-m-d H:i:s') . '</h1>';
+        
+
+    } else {
+        echo '<h1>You ' . $logged . ' at ' . date('Y-m-d H:i:s') . '.</h1>';
+    }
+    
+    ?>
+
+<?php
+$this->load->view($this->config->item('theme') . 'footer'); ?>
